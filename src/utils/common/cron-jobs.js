ad625763ad146289f76df9536bb2772be80d38d0
Piyush <b521044@iiit-bh.ac.in>
2024-07-18 14:22:21 +0530

    add idempotency key to make payment

--- src/utils/common/cron-jobs.js
+++ src/utils/common/cron-jobs.js
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 const { BookingService } = require('../../services');
 
 function scheduleCrons() {
-    cron.schedule('*/1 * * * *', async () => {
+    cron.schedule('*/30 * * * *', async () => {
         await BookingService.cancelOldBookings();
     });
 }
