9bb03226b40979e803b1ae329924a87a256ab96e
Piyush <b521044@iiit-bh.ac.in>
2024-07-23 11:55:03 +0530

    complete published mail event

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
-const express = require('express');
+const express = require("express");
+const amqplib = require("amqplib");
 
-const { ServerConfig } = require('./config');
-const apiRoutes = require('./routes');
-const CRONS = require('./utils/common/cron-jobs');
+// async function connectQueue() {
+//   try {
+//     const connection = await amqplib.connect("amqp://localhost");
+//     const channel = await connection.createChannel();
 
+//     await channel.assertQueue("noti-queue");
+//     setInterval(() => {
+//       channel.sendToQueue("noti-queue", Buffer.from(JSON.stringify(Hello)));
+//     }, 1000);
+//     await channel.sendToQueue("noti-queue", Buffer.from("Hello from RabbitMQ"));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// } 
 
+const { ServerConfig,Queue } = require("./config");
+const apiRoutes = require("./routes");
+const CRONS = require("./utils/common/cron-jobs");
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/bookingService/api',apiRoutes)
-app.use('/api', apiRoutes);
+app.use("/bookingService/api", apiRoutes);
+app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+// app.listen(ServerConfig.PORT, () => {
+//   console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+//   CRONS();
+// });
+
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
     CRONS();
+    await Queue.connectQueue();
+    console.log("queue connected")
 });
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const { StatusCodes } = require("http-status-codes");
 
 const { BookingRepository } = require("../repositories");
-const { ServerConfig } = require("../config");
+const { ServerConfig,Queue } = require("../config");
 const db = require("../models");
 const AppError = require("../utils/errors/app-error");
 const { Enums } = require("../utils/common");
@@ -68,11 +68,17 @@ async function makePayment(data) {
       );
     }
     // we assume here that payment is successful
-    await bookingRepository.update(
-      data.bookingId,
-      { status: BOOKED },
-      transaction
-    );
+    // await bookingRepository.update(
+    //   data.bookingId,
+    //   { status: BOOKED },
+    //   transaction
+    // );
+    await bookingRepository.update(data.bookingId, {status: BOOKED}, transaction);
+        Queue.sendData({
+            recepientEmail: 'anujsingh1234567892003@gmail.com',
+            subject: 'Flight booked',
+            text: `Booking successfully done for the booking ${data.bookingId}`
+        });
     await transaction.commit();
   } catch (error) {
     await transaction.rollback();
